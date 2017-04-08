76b0847088a7127efd3f99b563c22b7c76fb8226
remicmacs <rembourgeon@gmail.com>
2017-04-08 20:26:37 +0200

    TP3 marche bien reste export

--- TP3/shell2.c
+++ TP3/shell2.c
@@ -8,53 +8,51 @@
 #include <errno.h>
 #include "ligne_commande.h"
 
-
-/* Définition d'un type booléen */
-typedef enum{false, true} bool;
-
-/* Afficher une invite de commande */
+/* Détecter la commande de sortie, de changement de dossier ou d'exportation de variable d'environnement */
+int exitShell(char * b);
+int chDir(char * cmd);
+int exportEnv(char * cmd);
 void prompt();
 
-/* Détecter la commande de sortie ou de changement de dossier*/
-bool exitShell(char * b);
-bool chDir(char * cmd);
-
-int main (int argc, char **argv,char *argenv[]){
+int main(int argc, char** argv, char** argenv){
     pid_t child_pid;
     int child_status;
-    do {
+    
+    while(1){
         prompt();
         char ** ligne = lis_ligne();
         
-        /* On vérifie s'il y a bien une entrée */
         if (!ligne_vide(ligne)){
-            /* En premier lieu on attrappe la condition de sortie */
-            if (exitShell(ligne[0])) break;
-            //J'ai essayé d'implémenter un cd ..
+            fflush(stdin);
+            if (exitShell(ligne[0])){
+                exit(0);
+            }
+            
             if (chDir(ligne[0])){
                 
                 // Si la primitive cd est appelée on appelle chdir
                 chdir(ligne[1]);
                 
                 /* Et on change la variable qui stocke le chemin du répertoire courant */
-                char * cwd = calloc(255,1);
+                char * cwd = (char *) calloc(255,1);
                 getcwd(cwd,255);
                 setenv("PWD",cwd,1);
             } else {
                 
-                child_pid = fork();
-                
-                printf("child pid=%d\n", child_pid );
-                
-                // le processus enfant
-                if(child_pid == 0) {
+                if(exportEnv(ligne[0])){
+                    separeE
                     
-                    /* La commande demandée prend la place du processus fils */
-                    execvp(ligne[0], ligne);
+                }else{
+                    child_pid = fork();
                     
-                    /* Si execvp n'est pas correctement exécuté, alors on est toujours ici*/
-                    free(ligne);
-                    switch (errno) {
+                    // On est chez le fils
+                    if(child_pid == 0 ){
+                        /* La commande demandée prend la place du processus fils */
+                        execvp(ligne[0], ligne);
+                        
+                        /* Affichage de l'erreur : certes mais ça plante grave */
+                        
+                        /*switch (errno) {
                         case (E2BIG):
                         perror("Trop d'arguments");
                         break;
@@ -70,27 +68,21 @@ int main (int argc, char **argv,char *argenv[]){
                         default :
                         perror("Erreur de commande inconnue");
                         break;
-                    }
+                    }*/
                     exit(-1);
-                    
-                    // On est chez le père
                 } else {
-                    // On attend le retour du fils
-                    // Il y a peut-être des tests à faire.
-                    pid_t retourPid = waitpid(child_pid, &child_status, WNOHANG);
-                    printf("PID retour fils : %d\n", retourPid);
+                    waitpid(child_pid, &child_status, 0);
+                }
                 }
             }
         }
-    } while(true);
-    
+    }
     return 0;
 }
 
-/* Fonction qui met à jour l'invite de commande et qui l'affiche */
 void prompt(){
-    char * finPr = " $ ";
-    char * userName = calloc(strlen(getenv("USER")),1);
+    char finPr[] = " $ ";
+    char * userName = (char * )calloc(strlen(getenv("USER")),1);
     strcpy(userName, getenv("USER"));
     char * userNameAt = strcat(userName, "@");
     char * userNameAtDirectory = strcat(userNameAt, getenv("PWD"));
@@ -99,18 +91,14 @@ void prompt(){
     fflush(stdout);
 }
 
-bool exitShell(char * cmd){
-    return (strcmp(cmd, "exit") == 0) ? true : false ;
+int exitShell(char * cmd){
+    return (strcmp(cmd, "exit") == 0) ? 1 : 0 ;
 }
 
-bool chDir(char * cmd){
-    return (strcmp(cmd, "cd") == 0) ? true : false;
+int chDir(char * cmd){
+    return (strcmp(cmd, "cd") == 0) ? 1 : 0;
 }
 
-void prompt2(){
-    
-}
-
-void parserCmd(char * cmd){
-    
+int exportEnv(char * cmd){
+    return (strcmp(cmd, "export") == 0) ? 1 : 0 ;
 }
